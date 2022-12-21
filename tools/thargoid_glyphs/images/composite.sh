e40d61fd523b9592f288c45037c2c3a255036fa5
NoFoolLikeOne <nofool@paparomeo.uk>
2022-12-21 16:07:16 +0000

    Updated release nunber and date ,...also missed commits

--- tools/thargoid_glyphs/images/composite.sh
+++ tools/thargoid_glyphs/images/composite.sh
@@ -11,7 +11,7 @@ for inner in symbols/[0-9].png symbols/[0-9][0-9].png ;  do
 		outer_id=$(symbol $outer)
 		echo $outer_id-$inner_id.gif
 		convert -page 128x128+0+0 symbols/core.png -page 128x128+0+0 symbols/$inner_id.png -page 128x128+0+0 symbols/$outer_id.png +level-colors orange,black -background none -flatten composite_gifs/$outer_id-$inner_id.gif
-		convert -page 128x128+0+0 symbols/core.png -page 128x128+0+0 symbols/$inner_id.png -page 128x128+0+0 symbols/$outer_id.png  -background none -flatten composite_gifs/$outer_id-$inner_id.png
+		convert -page 128x128+0+0 symbols/core.png -page 128x128+0+0 symbols/$inner_id.png -page 128x128+0+0 symbols/$outer_id.png  -background none -flatten composite/$outer_id-$inner_id.png
 	done
 done
 
